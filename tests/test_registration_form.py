335afd9d14d8ca35403553a1292350ca21dc6fc3
Rustam <kodzokoff2016@yandex.ru>
2024-02-19 10:52:00 +0300

    убрал рекламу

--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -1,12 +1,18 @@
 from selene import browser, be, have, by
 import allure
+from selene.core import command
+
 import resource
+
 
 @allure.title("Registration form")
 def test_for_demoqa():
     with allure.step('Open form'):
         browser.open('https://demoqa.com/automation-practice-form')
         browser.element('[aria-label="Consent"]').click()
+        browser.all("[id^=google_ads][id$=container__]").with_(timeout=10).wait_until(
+            have.size_greater_than_or_equal(3))
+        browser.all("[id^=google_ads][id$=container__]").perform(command.js.remove)
 
     with allure.step('Fill form user data'):
         browser.element('#firstName').type('Светлана')
